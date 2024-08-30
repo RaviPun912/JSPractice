846bdc759e88aa985e4fdbb4804ba0cec408e881
Ravipun23 <rvpoon23@gmail.com>
2024-08-30 09:36:37 +0545

    updated comments

--- Day1-4/day2.js
+++ Day1-4/day2.js
@@ -1,1 +1,9 @@
-    
+//git init
+//git status //checks status
+//git add . //adds all files
+//git commit -m "first commit" //commit with message
+//git log //shows log
+//git branch -M main //adds main branch
+//git remote add origin https://github.com/username/repository.git //connects to github
+//git push -u origin main //pushes to github //-u is for first time push
+
