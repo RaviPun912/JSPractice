32f2fe05003b3903801eddb92413f074c0fb7d2e
Ravipun23 <rvpoon23@gmail.com>
2024-08-30 09:46:06 +0545

    updated comments

--- Day1-4/day2.js
+++ Day1-4/day2.js
@@ -7,3 +7,4 @@
 //git remote add origin https://github.com/username/repository.git //connects to github
 //git push -u origin main //pushes to github //-u is for first time push
 
+//git checkout -b NayaBranchNam //creating new branch
